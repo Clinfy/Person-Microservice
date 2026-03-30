47306f43171a24fc87f84702a325b7773a554a91
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2026-03-30 17:04:35 -0300

    Add new environment variables for Auth Service and optional application port setup

--- src/config/env-validation.ts
+++ src/config/env-validation.ts
@@ -1,10 +1,22 @@
 import { plainToInstance } from 'class-transformer';
-import { IsNotEmpty, IsString, validateSync } from 'class-validator';
+import { IsNotEmpty, IsNumber, IsOptional, IsString, validateSync } from 'class-validator';
 
 class EnvironmentVariables {
   @IsString()
   @IsNotEmpty()
   DATABASE_HOST: string;
+
+  @IsString()
+  @IsNotEmpty()
+  AUTH_SERVICE_URL: string;
+
+  @IsString()
+  @IsNotEmpty()
+  AUTH_SERVICE_API_KEY: string;
+
+  @IsNumber()
+  @IsOptional()
+  PORT: number;
 }
 
 export function validate(config: Record<string, unknown>) {
