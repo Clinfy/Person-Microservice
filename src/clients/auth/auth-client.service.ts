0a794638b8c0119b4f3900c171ad0bf62fc5095f
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2026-03-30 16:39:26 -0300

    Add methods to AuthClientService for permission checks, user retrieval, and endpoint permissions

---
+++ src/clients/auth/auth-client.service.ts
@@ -0,0 +1,69 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import axios from 'axios';
+import { Request } from 'express';
+import { getClientIp } from 'src/common/utils/get-client-ip.util';
+import { propagateAxiosError } from 'src/common/utils/propagate-axios-error';
+import { extractAuthToken } from 'src/common/utils/extract-bearer-token.util';
+import { AuthUser } from 'src/clients/auth/auth-client.interface';
+
+@Injectable()
+export class AuthClientService {
+  constructor(private readonly configService: ConfigService) {}
+
+  async canDo(permission: string, token: string, request: Request): Promise<boolean> {
+    const authApi = await this.axiosAuthApi(request);
+    const response = await authApi.get<boolean>(`/users/can-do/${permission}`, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+
+    return response.data;
+  }
+
+  async getMe(request: Request): Promise<AuthUser> {
+    const authApi = await this.axiosAuthApi(request);
+    const token = extractAuthToken(request);
+    const response = await authApi.get<AuthUser>('/users/me', {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+
+    return response.data;
+  }
+
+
+  async getEndpointPermissions(key: string, request: Request): Promise<string[]> {
+    const authApi = await this.axiosAuthApi(request);
+    const response = await authApi.get<string[]>(`/endpoint-permission-rules/get-endpoint-permissions/${key}`);
+
+    return response.data;
+  }
+
+  private async axiosAuthApi(request: Request) {
+    const baseUrl = this.configService.get<string>('AUTH_SERVICE_URL');
+    const apiKey = this.configService.get<string>('AUTH_SERVICE_API_KEY');
+
+    const authApi = axios.create({
+      baseURL: baseUrl,
+      timeout: 5000,
+    });
+
+    authApi.interceptors.request.use(
+      async (config) => {
+        config.headers['x-api-key'] = apiKey;
+        config.headers['content-type'] = 'application/json';
+        config.headers['x-forwarded-for'] = getClientIp(request);
+        config.headers['x-real-ip'] = getClientIp(request);
+        config.headers['user-agent'] = request.headers['user-agent'] || '';
+        return config;
+      },
+      (error) => propagateAxiosError(error),
+    );
+
+    authApi.interceptors.response.use(
+      (response) => response,
+      (error) => propagateAxiosError(error),
+    );
+
+    return authApi;
+  }
+}
