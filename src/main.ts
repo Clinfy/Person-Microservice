25b2126283381ed008f2c00c5923ee6712f3374c
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2026-03-30 16:52:49 -0300

    Set up global exception filter, input validation, and request proxy trust configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,46 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import cookieParser from 'cookie-parser';
+import { AllExceptionsFilter } from 'src/common/filters/all-exceptions.filter';
+import { useContainer } from 'class-validator';
+import { BadRequestException, HttpStatus, ValidationPipe } from '@nestjs/common';
+import { findFirstErrorCode, findFirstMessage } from 'src/common/utils/find-erros-data.util';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //Trust Proxy
+  const expressApp = app.getHttpAdapter().getInstance();
+  expressApp.set('trust proxy', true);
+
+  //Cookie Parser
+  app.use(cookieParser());
+
+  //Error Handler
+  const exceptionFilter = app.get(AllExceptionsFilter);
+  app.useGlobalFilters(exceptionFilter);
+
+  //Logs
+  useContainer(app.select(AppModule), { fallbackOnErrors: true }); // <—
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: { enableImplicitConversion: true },
+      validationError: { target: false, value: true },
+      exceptionFactory: (errors) => {
+        const errorCode = findFirstErrorCode(errors) ?? 'VALIDATION_ERROR';
+        const message = findFirstMessage(errors);
+        return new BadRequestException({
+          statusCode: HttpStatus.BAD_REQUEST,
+          errorCode,
+          message,
+        });
+      },
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
