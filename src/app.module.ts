428d64eedd55ec71025e5620cd696c1f01d83da2
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2026-03-30 16:53:01 -0300

    Integrate `WinstonModule` for logging, add `AuthClientModule`, and configure global exception filter

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,14 +5,21 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { validate } from 'src/config/env-validation';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { entities } from 'src/entities';
+import { WinstonModule } from 'nest-winston';
+import winston from 'winston';
+import 'winston-daily-rotate-file';
+import { AllExceptionsFilter } from 'src/common/filters/all-exceptions.filter';
+import { AuthClientModule } from 'src/clients/auth/auth-client.module';
 
 @Module({
   imports: [
+    //Config Module
     ConfigModule.forRoot({
       isGlobal: true,
-      validate
+      validate,
     }),
 
+    //TypeOrm Database Module
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
@@ -21,12 +28,39 @@ import { entities } from 'src/entities';
         url: configService.get<string>('DATABASE_HOST'),
         entities: [...entities],
         synchronize: true,
-      })
+      }),
     }),
 
-    TypeOrmModule.forFeature(entities)
+    //Winston Logger Module
+    WinstonModule.forRoot({
+      level: 'info',
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.errors({ stack: true }),
+        winston.format.json(),
+      ),
+      transports: [
+        //new winston.transports.Console(),
+        new winston.transports.DailyRotateFile({
+          filename: 'logs/error-%DATE%.log',
+          datePattern: 'YYYY-MM-DD',
+          level: 'error',
+          maxSize: '5m',
+          maxFiles: '14d',
+        }),
+        new winston.transports.DailyRotateFile({
+          filename: 'logs/combined-%DATE%.log',
+          datePattern: 'YYYY-MM-DD',
+          maxSize: '10m',
+          maxFiles: '30d',
+        }),
+      ],
+    }),
+
+    TypeOrmModule.forFeature(entities),
+    AuthClientModule
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AllExceptionsFilter],
 })
 export class AppModule {}
