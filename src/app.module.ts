9f3aa93505752ff8f79571851c4c9904bc05981c
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2026-03-30 16:05:19 -0300

    Integrate `@nestjs/config` and `TypeOrmModule` for configuration management and database connection setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { validate } from 'src/config/env-validation';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { entities } from 'src/entities';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validate
+    }),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get<string>('DATABASE_HOST'),
+        entities: [...entities],
+        synchronize: true,
+      })
+    })
+
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
