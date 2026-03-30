2f6e236387656154698a1dd521d1c2ecc4ddf950
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2026-03-30 15:51:07 -0300

    Export entities array including GenderEntity, OutboxEntity, and PersonEntity

---
+++ src/entities/index.ts
@@ -0,0 +1,9 @@
+import { GenderEntity } from 'src/entities/gender.entity';
+import { OutboxEntity } from 'src/entities/outbox.entity';
+import { PersonEntity } from 'src/entities/person.entity';
+
+export const entities = [
+  GenderEntity,
+  OutboxEntity,
+  PersonEntity
+];
