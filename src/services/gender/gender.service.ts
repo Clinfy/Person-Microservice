b0d502271680f3cb1d4b592d30d4df186bf9b1d8
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2026-03-30 21:30:38 -0300

    Add `create` method to `GenderService` with repository integration and error handling

--- src/services/gender/gender.service.ts
+++ src/services/gender/gender.service.ts
@@ -1,4 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
+import { GenderRepository } from 'src/services/gender/gender.repository';
+import { CreateGenderDto } from 'src/interfaces/dto/gender.dto';
+import { GenderEntity } from 'src/entities/gender.entity';
+import { GenderErrorCodes, GenderException } from 'src/services/gender/gender.exception';
 
 @Injectable()
-export class GenderService {}
+export class GenderService {
+  constructor(private readonly genderRepository: GenderRepository) {}
+
+  async create(dto: CreateGenderDto): Promise<GenderEntity> {
+    try {
+      return this.genderRepository.save(this.genderRepository.create(dto));
+    } catch (error) {
+      throw new GenderException('Gender creation failed', GenderErrorCodes.GENDER_NOT_CREATED, error.status ?? HttpStatus.INTERNAL_SERVER_ERROR, error );
+    }
+  }
+}
