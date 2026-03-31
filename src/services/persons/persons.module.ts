f0e33315814e6c2f55915633ccf73e5209d61bf9
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2026-03-31 14:34:03 -0300

    Add `GeorefModule` and `PersonsRepository` to `PersonsModule` for georeferencing and data access

--- src/services/persons/persons.module.ts
+++ src/services/persons/persons.module.ts
@@ -3,10 +3,12 @@ import { PersonsService } from './persons.service';
 import { PersonsController } from './persons.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PersonEntity } from 'src/entities/person.entity';
+import { GeorefModule } from 'src/clients/georef/georef.module';
+import { PersonsRepository } from 'src/services/persons/persons.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PersonEntity])],
+  imports: [TypeOrmModule.forFeature([PersonEntity]), GeorefModule],
   controllers: [PersonsController],
-  providers: [PersonsService],
+  providers: [PersonsService, PersonsRepository],
 })
 export class PersonsModule {}
