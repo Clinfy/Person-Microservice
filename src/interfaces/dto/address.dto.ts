884d3d496a2d6a023657e467386631fd32371d33
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2026-03-31 14:37:46 -0300

    Remove `AddressPatchDto` from `address.dto.ts` as it is no longer needed.

--- src/interfaces/dto/address.dto.ts
+++ src/interfaces/dto/address.dto.ts
@@ -21,25 +21,3 @@ export class AddressDto {
   @IsString({ message: 'Locality input must be an string' })
   locality: string;
 }
-
-export class AddressPatchDto {
-  @IsOptional()
-  @IsString({ message: 'This input must be an string' })
-  street_one?: string;
-
-  @IsOptional()
-  @IsNumber({}, { message: 'This input must be a number' })
-  street_number?: number;
-
-  @IsOptional()
-  @IsString({ message: 'This input must be an string' })
-  street_two?: string;
-
-  @IsNotEmpty({ message: 'This field is required' })
-  @IsString({ message: 'This input must be an string' })
-  province: string;
-
-  @IsNotEmpty({ message: 'This field is required' })
-  @IsString({ message: 'This input must be an string' })
-  locality: string;
-}
