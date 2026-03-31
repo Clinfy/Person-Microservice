a2d99969748704eb9aef26b4140625a8ff34181e
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2026-03-31 14:34:12 -0300

    Inject `PersonsRepository` and `GeorefService` into `PersonsService` for data access and georeferencing.

--- src/services/persons/persons.service.ts
+++ src/services/persons/persons.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { PersonsRepository } from 'src/services/persons/persons.repository';
+import { GeorefService } from 'src/clients/georef/georef.service';
 
 @Injectable()
-export class PersonsService {}
+export class PersonsService {
+  constructor(
+    private readonly personsRepository: PersonsRepository,
+    private readonly georefService: GeorefService,
+  ) {}
+}
