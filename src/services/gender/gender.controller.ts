bbc2962df1dcb1a19c5bef216b2138d1f36cae1f
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2026-03-31 00:07:55 -0300

    Add `delete`, `findOne`, and `findAll` methods to `GendersController`

--- src/services/gender/gender.controller.ts
+++ src/services/gender/gender.controller.ts
@@ -1,9 +1,10 @@
-import { Body, Controller, Param, Patch, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query, UseGuards } from '@nestjs/common';
 import { GendersService } from 'src/services/gender/gender.service';
 import { AuthGuard } from 'src/common/guards/auth.guard';
 import { EndpointKey } from 'src/common/decorators/endpoint-key.decorator';
 import { CreateGenderDto } from 'src/interfaces/dto/gender.dto';
 import { GenderEntity } from 'src/entities/gender.entity';
+import { PaginatedResponseDto, PaginationQueryDto } from 'src/interfaces/dto/pagination.dto';
 
 @Controller('genders')
 export class GendersController {
@@ -22,5 +23,27 @@ export class GendersController {
   edit (@Param('id') id: string, @Body() dto: CreateGenderDto): Promise<GenderEntity> {
     return this.genderService.edit(id, dto);
   }
+
+  @UseGuards(AuthGuard)
+  @EndpointKey('genders.delete')
+  @Delete('delete/:id')
+  delete (@Param('id') id: string): Promise<{message: string}> {
+    return this.genderService.delete(id);
+  }
+
+  @UseGuards(AuthGuard)
+  @EndpointKey('genders.find')
+  @Get('find/:id')
+  findOne (@Param('id') id: string): Promise<GenderEntity> {
+    return this.genderService.findOneById(id);
+  }
+
+  @UseGuards(AuthGuard)
+  @EndpointKey('genders.find')
+  @Get('all')
+  findAll (@Query() query: PaginationQueryDto): Promise<PaginatedResponseDto<GenderEntity>> {
+    return this.genderService.findAll(query);
+  }
+
 
 }
