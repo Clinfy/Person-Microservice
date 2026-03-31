a1e9f11ff7fc6ca5bb5cd3210f46b817109c5d67
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2026-03-31 14:33:34 -0300

    Inject `PersonsService` into `PersonsController` for service integration.

--- src/services/persons/persons.controller.ts
+++ src/services/persons/persons.controller.ts
@@ -1,4 +1,7 @@
 import { Controller } from '@nestjs/common';
+import { PersonsService } from 'src/services/persons/persons.service';
 
 @Controller('persons')
-export class PersonsController {}
+export class PersonsController {
+  constructor(private readonly personsService: PersonsService) {}
+}
