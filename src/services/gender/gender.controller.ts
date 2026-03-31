4e4aa12d61b971ddae0867e63d933c75592e3d71
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2026-03-30 21:30:35 -0300

    Add `create` endpoint to `GenderController` with authentication and input validation

--- src/services/gender/gender.controller.ts
+++ src/services/gender/gender.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { GenderService } from 'src/services/gender/gender.service';
+import { AuthGuard } from 'src/common/guards/auth.guard';
+import { EndpointKey } from 'src/common/decorators/endpoint-key.decorator';
+import { CreateGenderDto } from 'src/interfaces/dto/gender.dto';
+import { GenderEntity } from 'src/entities/gender.entity';
 
 @Controller('gender')
-export class GenderController {}
+export class GenderController {
+  constructor(private readonly genderService: GenderService) {}
+
+  @UseGuards(AuthGuard)
+  @EndpointKey('gender.create')
+  @Post('create')
+  create(@Body() dto: CreateGenderDto): Promise<GenderEntity> {
+    return this.genderService.create(dto);
+  }
+}
