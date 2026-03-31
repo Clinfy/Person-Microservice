7ba953b335b86faed023821b0d8cb1f725c2df21
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2026-03-31 00:00:34 -0300

    Add CRUD methods to `GenderService`: create, edit, delete, find one, and find all

--- src/services/gender/gender.service.ts
+++ src/services/gender/gender.service.ts
@@ -1,9 +1,10 @@
 import { HttpStatus, Injectable } from '@nestjs/common';
 import { GenderRepository } from 'src/services/gender/gender.repository';
-import { CreateGenderDto } from 'src/interfaces/dto/gender.dto';
+import { CreateGenderDto, PatchGenderDto } from 'src/interfaces/dto/gender.dto';
 import { GenderEntity } from 'src/entities/gender.entity';
 import { GenderErrorCodes, GenderException } from 'src/services/gender/gender.exception';
 import { RequestContextService } from 'src/common/context/request-context.service';
+import { PaginatedResponseDto, PaginationQueryDto } from 'src/interfaces/dto/pagination.dto';
 
 @Injectable()
 export class GenderService {
@@ -27,6 +28,57 @@ export class GenderService {
         error.status ?? HttpStatus.INTERNAL_SERVER_ERROR,
         error,
       );
+    }
+  }
+
+  async edit (id:string, dto: PatchGenderDto): Promise<GenderEntity> {
+    try {
+      const gender = await this.findOneById(id);
+      return await this.genderRepository.save(await this.genderRepository.merge(gender,dto))
+    } catch (error) {
+      throw new GenderException(
+        'Gender update failed',
+        GenderErrorCodes.GENDER_NOT_UPDATED,
+        error.status ?? HttpStatus.INTERNAL_SERVER_ERROR,
+        error,
+      );
+    }
+  }
+
+  async delete(id: string): Promise<{message: string}> {
+    try {
+      const gender = await this.findOneById(id);
+      await this.genderRepository.remove(gender);
+      return {message: `Gender ${gender.display_name} (${gender.code}) deleted successfully`}
+    } catch (error) {
+      throw new GenderException(
+        'Gender deletion failed',
+        GenderErrorCodes.GENDER_NOT_DELETED,
+        error.status ?? HttpStatus.INTERNAL_SERVER_ERROR,
+        error,
+      );
+    }
+  }
+
+  async findOneById(id: string): Promise<GenderEntity> {
+    const gender = await this.genderRepository.findOneById(id);
+
+    if(gender) return gender;
+
+    throw new GenderException(
+      'Gender not found',
+      GenderErrorCodes.GENDER_NOT_FOUND,
+      HttpStatus.NOT_FOUND,
+    );
+  }
+
+  async findAll(query: PaginationQueryDto): Promise<PaginatedResponseDto<GenderEntity>> {
+    try {
+      const [data, total] = await this.genderRepository.findAll(query);
+      return new PaginatedResponseDto(data, total, query.page, query.limit);
+    } catch (error) {
+      throw new GenderException('Genders not found', error.status ?? GenderErrorCodes.GENDER_NOT_FOUND, HttpStatus.NOT_FOUND, error);
+
     }
   }
 }
