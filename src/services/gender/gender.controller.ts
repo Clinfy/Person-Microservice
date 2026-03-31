605a24ca8b8336c80d243a44eabae4425155215b
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2026-03-31 00:03:34 -0300

    Refactor `Gender` controller: rename to `GendersController` and update service import

--- src/services/gender/gender.controller.ts
+++ src/services/gender/gender.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Param, Patch, Post, UseGuards } from '@nestjs/common';
-import { GenderService } from 'src/services/gender/gender.service';
+import { GendersService } from 'src/services/gender/gender.service';
 import { AuthGuard } from 'src/common/guards/auth.guard';
 import { EndpointKey } from 'src/common/decorators/endpoint-key.decorator';
 import { CreateGenderDto } from 'src/interfaces/dto/gender.dto';
@@ -7,7 +7,7 @@ import { GenderEntity } from 'src/entities/gender.entity';
 
 @Controller('genders')
 export class GendersController {
-  constructor(private readonly genderService: GenderService) {}
+  constructor(private readonly genderService: GendersService) {}
 
   @UseGuards(AuthGuard)
   @EndpointKey('genders.create')
