e177d9acfb6f144ecca6dd069c8340563eb8190e
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2026-03-31 11:29:37 -0300

    Add Jest config file and update test scripts to leverage TypeScript configuration. Update dependencies with test-related packages.

---
+++ jest.config.ts
@@ -0,0 +1,20 @@
+module.exports = {
+  preset: 'ts-jest',
+
+  testEnvironment: 'node',
+
+  testMatch: [
+    '<rootDir>/src/**/*.spec.ts',
+    '<rootDir>/test/integration/*.spec.ts',
+  ],
+
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/src/$1',
+  },
+
+  transformIgnorePatterns: ['/node_modules/(?!uuid)'],
+
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+};
