dfbaf9d0d159f9c1290454b25fedc45537434d39
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2026-03-31 11:38:01 -0300

    Initialize `Persons` module with controller, service, module, and basic tests.

---
+++ src/services/persons/persons.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { PersonsService } from './persons.service';
+import { PersonsController } from './persons.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PersonEntity } from 'src/entities/person.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([PersonEntity])],
+  controllers: [PersonsController],
+  providers: [PersonsService],
+})
+export class PersonsModule {}
