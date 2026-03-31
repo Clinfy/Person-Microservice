26f49ccdc8ebbc24b4090c73c030c3b7701b8a7b
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2026-03-30 22:15:57 -0300

    Add `created_by` field population using `RequestContextService` in `GenderService.create`

--- src/services/gender/gender.service.ts
+++ src/services/gender/gender.service.ts
@@ -3,16 +3,30 @@ import { GenderRepository } from 'src/services/gender/gender.repository';
 import { CreateGenderDto } from 'src/interfaces/dto/gender.dto';
 import { GenderEntity } from 'src/entities/gender.entity';
 import { GenderErrorCodes, GenderException } from 'src/services/gender/gender.exception';
+import { RequestContextService } from 'src/common/context/request-context.service';
 
 @Injectable()
 export class GenderService {
-  constructor(private readonly genderRepository: GenderRepository) {}
+  constructor(
+    private readonly genderRepository: GenderRepository,
+    private readonly contextService: RequestContextService,
+  ) {}
 
   async create(dto: CreateGenderDto): Promise<GenderEntity> {
     try {
-      return this.genderRepository.save(this.genderRepository.create(dto));
+      return this.genderRepository.save(
+        this.genderRepository.create({
+          ...dto,
+          created_by: this.contextService.getCurrentUser()
+        }),
+      );
     } catch (error) {
-      throw new GenderException('Gender creation failed', GenderErrorCodes.GENDER_NOT_CREATED, error.status ?? HttpStatus.INTERNAL_SERVER_ERROR, error );
+      throw new GenderException(
+        'Gender creation failed',
+        GenderErrorCodes.GENDER_NOT_CREATED,
+        error.status ?? HttpStatus.INTERNAL_SERVER_ERROR,
+        error,
+      );
     }
   }
 }
