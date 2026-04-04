3ad9d96a320b38af57681f3aa7ae81535cd526bc
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2026-04-04 16:28:07 -0300

    Add `getPersonDetails` and `getBatchPersonDetails` endpoints to `PersonsController` with error handling

--- src/services/persons/persons.controller.ts
+++ src/services/persons/persons.controller.ts
@@ -1,7 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, Param, ParseUUIDPipe, Post } from '@nestjs/common';
 import { PersonsService } from 'src/services/persons/persons.service';
+import { PersonErrorCodes, PersonException } from 'src/services/persons/persons.exception';
+import { IPerson } from 'src/interfaces/person.interface';
+import { BatchPersonDetailsDto } from 'src/interfaces/dto/person.dto';
 
 @Controller('persons')
 export class PersonsController {
   constructor(private readonly personsService: PersonsService) {}
+
+  @Get(':id/details')
+  async getPersonDetails(@Param('id', ParseUUIDPipe) id: string): Promise<IPerson> {
+    const person = await this.personsService.getPersonDetails(id);
+    if (!person) throw new PersonException('Person not found', PersonErrorCodes.PERSON_NOT_FOUND, HttpStatus.NOT_FOUND);
+    return person;
+  }
+
+  @Post('details/batch')
+  async getBatchPersonDetails(@Body() dto: BatchPersonDetailsDto): Promise<Record<string, IPerson>> {
+    return this.personsService.getBatchPersonDetails(dto.ids);
+  }
 }
