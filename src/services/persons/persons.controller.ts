fb2d2ae8b4181351f7bd37ee0b78c7955772e6f3
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2026-04-06 16:17:35 -0300

    Secure `getPersonDetails` and `getBatchPersonDetails` endpoints with `AuthGuard` and add `EndpointKey` decorator.

--- src/services/persons/persons.controller.ts
+++ src/services/persons/persons.controller.ts
@@ -1,20 +1,24 @@
-import { Body, Controller, Get, HttpStatus, Param, ParseUUIDPipe, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseUUIDPipe, Post, UseGuards } from '@nestjs/common';
 import { PersonsService } from 'src/services/persons/persons.service';
-import { PersonErrorCodes, PersonException } from 'src/services/persons/persons.exception';
 import { IPerson } from 'src/interfaces/person.interface';
 import { BatchPersonDetailsDto } from 'src/interfaces/dto/person.dto';
+import { AuthGuard } from 'src/common/guards/auth.guard';
+import { EndpointKey } from 'src/common/decorators/endpoint-key.decorator';
 
 @Controller('persons')
 export class PersonsController {
   constructor(private readonly personsService: PersonsService) {}
 
+
+  @UseGuards(AuthGuard)
+  @EndpointKey('persons.details')
   @Get(':id/details')
   async getPersonDetails(@Param('id', ParseUUIDPipe) id: string): Promise<IPerson> {
-    const person = await this.personsService.getPersonDetails(id);
-    if (!person) throw new PersonException('Person not found', PersonErrorCodes.PERSON_NOT_FOUND, HttpStatus.NOT_FOUND);
-    return person;
+    return await this.personsService.getPersonDetails(id);
   }
 
+  @UseGuards(AuthGuard)
+  @EndpointKey('persons.details')
   @Post('details/batch')
   async getBatchPersonDetails(@Body() dto: BatchPersonDetailsDto): Promise<Record<string, IPerson>> {
     return this.personsService.getBatchPersonDetails(dto.ids);
